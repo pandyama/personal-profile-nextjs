6799e9bf52b0a7000e6a4976ecf41ff1ae655608
Meet Pandya <pandyama2@gmail.com>
2025-05-19 22:17:13 -0600

    header component as with nav

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import styles from './index.module.scss'
+
+type HeaderProps = {
+  title?: string
+  showBackButton?: boolean
+}
+
+const Header = ({ title }: HeaderProps) => {
+  console.log('🚀 ~ Header ~ title:', title)
+  return (
+    <div className={styles.headerContent}>
+      <h1>Meet Pandya</h1>
+
+      <div className={styles.navContainer}>
+        <nav className={styles.nav}>
+          <Link href='/'>Home</Link>
+          <Link href='/experience'>Experience</Link>
+          {/* <Link href='/experience'>Projects</Link> */}
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+export default Header
