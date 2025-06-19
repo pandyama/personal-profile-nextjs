396ba24ea9039ca46262c2fe3a199929b8e2959c
pandyama <pandyama2@gmail.com>
2025-06-18 22:28:03 -0600

    projects app added and some styles updated

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,6 @@
 // import Link from 'next/link'
 import React from '../../../public/icons/react'
-// import styles from './page.module.scss'
+import styles from './page.module.scss'
 import Page from '@/app/components/Page'
 import Card from '../components/Card'
 
@@ -9,13 +9,36 @@ type HeaderProps = {
   showBackButton?: boolean
 }
 
+/**
+ * Projects to show
+ * - Yukold
+ * - Jotdown
+ * - Mailmain
+ */
+
 const Projects = ({ title }: HeaderProps) => {
   console.log('🚀 ~ Projects', title)
   return (
     <div>
       <Page />
-      Projects
-      <Card title={'Yukold'} description='Testing' />
+      <div className={styles.GridContainer}>
+        <div className={styles.CardGrid}>
+          <Card
+            title={'Yukold'}
+            description={
+              'Mobile app that provides weather forecast by searching for a city'
+            }
+          />
+          <Card
+            title={'Jotdown'}
+            description='Mobile app that allows users to create notes'
+          />
+          <Card
+            title={'Mailman'}
+            description='Simple web app that allows users to make API calls much like a popular app out there'
+          />
+        </div>
+      </div>
     </div>
   )
 }
