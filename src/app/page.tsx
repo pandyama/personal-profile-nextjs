42ad4d49673454530f2b9489b9bacf3816c9c525
pandyama <pandyama2@gmail.com>
2025-07-14 21:34:29 -0600

    added projects grid on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,34 @@
+import Card from './components/Card'
 import Layout from './components/Layout'
-import styles from './page.module.css'
+import styles from './page.module.scss'
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Layout>
-        <h1> &nbsp; Hello, I am a Fullstack Developer</h1>
+        <h1>&nbsp; Hello, I am a Fullstack Developer</h1>
+        <h2>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;i enjoy working on fullstack web and
+          mobile apps.
+        </h2>
+        <div className={styles.GridContainer}>
+          <div className={styles.CardGrid}>
+            <Card
+              title={'Yukold'}
+              description={
+                'Mobile app that provides weather forecast by searching for a city'
+              }
+            />
+            <Card
+              title={'Jotdown'}
+              description='Mobile app that allows users to create notes'
+            />
+            {/* <Card
+            title={'Mailman'}
+            description='Simple web app that allows users to make API calls much like a popular app out there'
+          /> */}
+          </div>
+        </div>
       </Layout>
     </div>
   )
