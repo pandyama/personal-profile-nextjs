3b5965e61b6b866ad4b920634b879bb22c988d78
pandyama <pandyama2@gmail.com>
2025-07-20 11:28:21 -0600

    card component handles button related props

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -3,16 +3,21 @@ import styles from './index.module.scss'
 type HeaderProps = {
   title?: string
   description?: string
-  showBackButton?: boolean
+  buttonText?: string
+  buttonLink?: string
 }
 
-const Card = ({ title, description }: HeaderProps) => {
+const Card = ({ title, description, buttonText, buttonLink }: HeaderProps) => {
   console.log('🚀 ~ Header ~ Card:', title)
   return (
     <div className={styles.card}>
       <h2>{title}</h2>
       <p>{description}</p>
-      <button className={styles.button}>Test</button>
+      <button className={styles.button}>
+        <a href={buttonLink} target='_blank' rel='noopener noreferrer'>
+          {buttonText}
+        </a>
+      </button>
       {/* <Button variant={'outline'}> Test</Button> */}
     </div>
   )
